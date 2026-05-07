c5888614ba921758423f9ed657906a90023e053e
uditha-cloud <udithayadavnamaswamy@gmail.com>
2026-05-07 08:44:15 +0530

    Add files via upload

---
+++ FixTimer.js
@@ -0,0 +1,6 @@
+let name = "Anusha";
+let marks = 85;
+
+// Using template literals
+let message = 'Hello ${name}, your marks are ${marks}';
+console.log(message);
---
+++ spread&rest.js
@@ -0,0 +1,4 @@
+let a=[1,2,3]
+let b=[4,5,6]
+let merge=[...a,...b]
+console.log(merge)y
